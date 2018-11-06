4f7aad38ab3d2567f9cfff426f1d49d6c3a591de
German Krauss <german@devartis.com>
2018-11-06 12:01:01 -0300

    Add gfortran and lapack for scikit-learn

--- rebuild.sh
+++ rebuild.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker build -t="devartis/python3-dev:1.0.1" python3-dev || exit 1
-# docker push devartis/python3-dev
+docker build -t="devartis/python3-dev:1.0.2" python3-dev || exit 1
+#docker push devartis/python3-dev
 
